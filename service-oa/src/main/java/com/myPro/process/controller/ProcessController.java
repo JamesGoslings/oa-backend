4ad4420b0aee8a9dc544723d4ed4377f5e5a61b0
JamesGosing <3248175240@qq.com>
2024-05-20 00:01:15 +0800

    perf: 优化了条件查询，增加了可选中的条件

--- service-oa/src/main/java/com/myPro/process/controller/ProcessController.java
+++ service-oa/src/main/java/com/myPro/process/controller/ProcessController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin/process")
+@RequestMapping("/admin/process/main")
 public class ProcessController {
 
     @Autowired
